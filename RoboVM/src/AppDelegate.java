615905f93556ba975d1fc47b3a9c5627308ef231
Harry Cheung <harry.cheung@gmail.com>
2015-02-13 08:37:43 -0800

    RoboVM app

---
+++ RoboVM/src/AppDelegate.java
@@ -0,0 +1,29 @@
+//
+// Copyright (c) 2015 Harry Cheung
+//
+
+import org.robovm.apple.foundation.*;
+import org.robovm.apple.uikit.*;
+
+public class AppDelegate extends UIApplicationDelegateAdapter {
+
+  private UIWindow window = null;
+
+  @Override
+  public boolean didFinishLaunching(UIApplication application, 
+  		UIApplicationLaunchOptions launchOptions) {
+    
+    window = new UIWindow(UIScreen.getMainScreen().getBounds());
+    window.setRootViewController(new MainViewController());
+    window.makeKeyAndVisible();
+    addStrongRef(window);
+    
+    return true;
+  }
+
+  public static void main(String[] args) {
+  	try (NSAutoreleasePool pool = new NSAutoreleasePool()) {
+      UIApplication.main(args, null, AppDelegate.class);
+  	}
+  }
+}
