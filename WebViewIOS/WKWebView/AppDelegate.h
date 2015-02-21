d4802e63835b841aa5a17169e6c5d5e65fc4bcb2
Harry Cheung <harry.cheung@gmail.com>
2015-02-20 17:10:15 -0800

    WebViewIOS WKWebView app

---
+++ WebViewIOS/WKWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKWebView
+//
+//  Created by harry on 2/20/15.
+//  Copyright (c) 2015 Harry Cheung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
