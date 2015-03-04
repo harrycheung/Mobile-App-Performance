6e549e2bee17d4488146ee28f998ca3a63fae657
Harry Cheung <harry.cheung@gmail.com>
2015-03-04 12:24:29 -0800

    Unnecessary window creation

--- Xamarin/XamarinIOS/AppDelegate.cs
+++ Xamarin/XamarinIOS/AppDelegate.cs
@@ -10,16 +10,6 @@ namespace XamarinIOS
 	[Register("AppDelegate")]
 	public partial class AppDelegate : UIApplicationDelegate
 	{
-		UIWindow window;
-
-		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
-		{
-			window = new UIWindow(UIScreen.MainScreen.Bounds);
-			window.MakeKeyAndVisible();
-			
-			return true;
-		}
-
 		public override UIWindow Window {
 			get;
 			set;
