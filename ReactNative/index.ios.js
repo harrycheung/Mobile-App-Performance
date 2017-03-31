fbf249323ee58ded813e0e73e636704c31f40894
James Thompson <james@mavenlink.com>
2017-03-31 16:20:30 -0600

    Updated iOS entrypoint for React Native example.

--- ReactNative/index.ios.js
+++ ReactNative/index.ios.js
@@ -11,8 +11,24 @@ import {
   Text,
   View
 } from 'react-native';
+import Benchmark from './common/benchmark'
 
 export default class ReactNativeBenchmark extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { results1000: 'None', results10000: 'None' };
+
+    this.runBenchmarks1000 = () => {
+      const results = Benchmark.run(1000);
+      this.setState({ results1000: results });
+    }
+
+    this.runBenchmarks10000 = () => {
+      const results = Benchmark.run(10000);
+      this.setState({ results10000: results });
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -20,11 +36,29 @@ export default class ReactNativeBenchmark extends Component {
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          To get started, edit index.android.js
         </Text>
         <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+
+        <Button
+          onPress={this.runBenchmarks1000}
+          title="Run Benchmarks (1,000)"
+          color="#841584"
+          />
+        <Text style={styles.instructions}>
+          Results (1,000): {this.state.results1000}
+        </Text>
+
+        <Button
+          onPress={this.runBenchmarks10000}
+          title="Run Benchmarks (10,000)"
+          color="#FF1584"
+          />
+        <Text style={styles.instructions}>
+          Results (10,000): {this.state.results10000}
         </Text>
       </View>
     );
